0e374b880b551a05499cdded8d3fdcb7af431999
AlexeyCodes <147306635+AlexeyCodes@users.noreply.github.com>
2024-08-01 11:47:29 +0300

    Add files via upload

---
+++ Счастливый.pas
@@ -0,0 +1,10 @@
+﻿program p1;
+var x, i, n: Integer;
+begin 
+  readln(n);
+  for i := 1 to n do
+  begin
+    readln(x);
+    if(x div 100000) + ((x div 10000) mod 10) + ((x mod 10000) div 1000) = ((x mod 1000) div 100) + ((x mod 100) div 10) + (x mod 10) then writeln('YES') else writeln('NO');  
+  end;
+end.
