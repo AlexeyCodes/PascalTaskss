f9876d6327948094d72d544177e277d0339967a4
AlexeyCodes <147306635+AlexeyCodes@users.noreply.github.com>
2024-08-01 11:47:17 +0300

    Add files via upload

---
+++ Строки.pas
@@ -0,0 +1,21 @@
+﻿program p1;
+var  x,y,i,j :longint;
+  
+  begin
+    readln(x);
+    
+    for i := 1 to x do
+    begin
+      if i mod 2 = 1 then 
+      begin
+       j := i * (-1);
+       y := y + j;  
+       end
+      else 
+      begin
+        j := i;
+        y := y + j;
+        end; 
+      end;
+      write(y);
+  end.
